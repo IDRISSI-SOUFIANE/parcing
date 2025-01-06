6047a43aa6039245d0d8bef6e52fd1c6fb589072
SOUFIANE IDRISSI <idrissisoufiane2000@gmail.com>
2025-01-07 00:51:28 +0100

    change another

--- parcing.c
+++ parcing.c
@@ -8,12 +8,12 @@ int	ft_check_parcing(char *res )
 {
 
 	if (!(ft_atoi(res)))
-		return (free(res), 0);
+		return ( 0); // if you free res, you will lose the first address, so will lose the rest of the address ()
 	else
 	{
 		printf("here -> (%ld)", (ft_atoi(res)));
+		// free(res);
 	}
-	free(res);
 	return (1);
 }
 
@@ -21,17 +21,28 @@ int	ft_argc(int ac, char **av)
 {
 	int		i;
 	int		j;
+	int		k;
 	char	**result;
 
 	i = 1;
 	while (i < ac)
 	{
-		result = ft_split(av[i], ' ');
+		result = ft_split(av[i], ' '); // "l 1 2 3" => free 'l' lose the last numbers
+		k = 0;
+		while (result[k])
+			k++;
 		j = 0;
 		while (result[j])
 		{
-			if (!(ft_check_parcing(result[j])))
-				return (free(result), 0);
+			if (!(ft_check_parcing(result[j]))) // the probleme is he free 'first char' and lose the reset
+			{
+				j = 0;
+				while (j < k)
+				{
+					free(result[j]);
+					j++;
+				}
+			}
 			else
 				j++;
 		}
