d76429ecf09a760b6a031e85b1017488beccba72
SOUFIANE IDRISSI <idrissisoufiane2000@gmail.com>
2025-01-07 10:33:40 +0100

    tomorow

--- parcing.c
+++ parcing.c
@@ -4,52 +4,38 @@ void f()
 {
 	system("leaks check");
 }
-int	ft_check_parcing(char *res )
+int	ft_check_parcing(char **res)
 {
+	int	i;
+	int	j;
 
-	if (!(ft_atoi(res)))
-		return ( 0); // if you free res, you will lose the first address, so will lose the rest of the address ()
-	else
+	j = 0;
+	while (res[j])
+		j++;
+	i = 0;
+	while (res[i])
 	{
-		printf("here -> (%ld)", (ft_atoi(res)));
-		// free(res);
+		if (!(ft_atoi(res[i])))
+			return (ft_free(res, j), free(res), 0);
+		else
+			printf("(%ld) -> ", ft_atoi(res[i]));
+		i++;
 	}
+	ft_free(res, i);
 	return (1);
 }
 
 int	ft_argc(int ac, char **av)
 {
 	int		i;
-	int		j;
-	int		k;
 	char	**result;
 
-	i = 1;
+	i = 1; // 1 < 4
 	while (i < ac)
 	{
 		result = ft_split(av[i], ' '); // "l 1 2 3" => free 'l' lose the last numbers
-		k = 0;
-		while (result[k])
-			k++;
-		j = 0;
-		while (result[j])
-		{
-			if (!(ft_check_parcing(result[j]))) // the probleme is he free 'first char' and lose the reset
-			{
-				j = 0;
-				while (j < k)
-				{
-					free(result[j]);
-					j++;
-				}
-			}
-			else
-			{
-				j++;
-				free(result[j]);
-			}
-		}
-		free(result);
+		if (!(ft_check_parcing(result)))
+			return (result = NULL, 0);
 		i++;
 	}
 	// free(result);
@@ -62,6 +48,66 @@ int main(int ac, char **av)
 	if (ac == 1 || (ac == 2 && !av[1][0]))
         return (1);
 	if(!(ft_argc(ac, av)))
+	{
+		printf("I am heren\n");
 		return (0);
+	}
     return (0);
 }
+
+
+
+
+// int	ft_argc(int ac, char *av)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		k;
+// 	char	**result;
+
+// 	i = 1; // 1 < 4
+// 	while (i < ac)
+// 	{
+// 		result = ft_split(av[i], ' '); // "l 1 2 3" => free 'l' lose the last numbers
+// 		ft_check_parcing(result);
+// 		i++;
+// 	}
+// 	// free(result);
+// 	return (1);
+// }
+
+	// int		i;
+	// int		j;
+	// int		k;
+	// char	**result;
+
+	// i = 1;
+	// while (i < ac)
+	// {
+	// 	result = ft_split(av[i], ' '); // "l 1 2 3" => free 'l' lose the last numbers
+	// 	k = 0;
+	// 	while (result[k])
+	// 		k++;
+	// 	j = 0;
+	// 	while (result[j])
+	// 	{
+	// 		if (!(ft_check_parcing(result[j]))) // the probleme is he free 'first char' and lose the reset
+	// 		{
+	// 			j = 0;
+	// 			while (j < k)
+	// 			{
+	// 				free(result[j]);
+	// 				j++;
+	// 			}
+	// 		}
+	// 		else
+	// 		{
+	// 			j++;
+	// 			free(result[j]);
+	// 		}
+	// 	}
+	// 	free(result);
+	// 	i++;
+	// }
+	// // free(result);
+	// return (1);
